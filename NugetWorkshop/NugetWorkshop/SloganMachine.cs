9af3b1bf57d6f74fedfa1eacf06277571d5fcc24
Samuel Bittmann <sbittmann@bluewin.ch>
2019-10-18 12:21:36 +0200

    Typo

--- NugetWorkshop/NugetWorkshop/SloganMachine.cs
+++ NugetWorkshop/NugetWorkshop/SloganMachine.cs
@@ -4,7 +4,7 @@ namespace NugetWorkshop
     {
         public string GenerateSlogan()
         {
-            //I was tasked with creating the slogan machine but none of my team have any idea how to do this. Besides,
+            //I was tasked with creating the slogan machine but noone of my team have any idea how to do this. Besides,
             //we really have no time for that, we're supposed to release next week!!!
             return "I have no clue how to do that";
         }
